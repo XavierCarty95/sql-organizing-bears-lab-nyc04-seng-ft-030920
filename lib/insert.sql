2bf154b7ae9347e5582d481e71cad0b102b30aa4
Xavier Carty <xaviercarty@gmail.com>
2020-03-22 16:59:36 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name , age , gender , color , temperament , alive) VALUES ('Mr.Chocolate', 20 , 'M' , 'Blue' , "Anger", 1 );
+INSERT INTO bears(name , age , gender , color , temperament , alive) VALUES ('Rowdy', 20 , 'M' , 'F' , "Anger", 0 );
+INSERT INTO bears(name , age , gender , color , temperament , alive) VALUES ('Tabitha', 20 , 'M' , 'dark brown ' , "Anger", 1);
+INSERT INTO bears(name , age , gender , color ,temperament , alive) VALUES ('Sergeant Brown', 20 , 'F' , 'Blue' , "Aggressive", 1 );
+INSERT INTO bears(name , age , gender , color , temperament , alive) VALUES ('Melissa', 20 , 'M' , 'dark brown' , "Anger", 1 );
+INSERT INTO bears(name , age , gender , color , temperament , alive) VALUES ('Grinch', 20 , 'M' , 'Brown' , "Anger", 0 );
+INSERT INTO bears(name , age , gender , color , temperament , alive) VALUES ('Wendy', 20 , 'M' , 'Blue' , "Anger", 0 );
+INSERT INTO bears(name , age , gender , color , temperament , alive) VALUES ( NULL , 20 , 'M' , 'Blue' , "Anger", 1 );
